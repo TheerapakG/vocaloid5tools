1fbcb9b33b9541bfed2e89bcd49ee4e4b53acc96
TheerapakG <theerapakg@gmail.com>
2018-08-21 22:45:42 +0700

    Add files via upload

--- pyvocaloid/WVSMModuleIF.py
+++ pyvocaloid/WVSMModuleIF.py
@@ -1,16 +1,59 @@
+#WIVSMSequenceManager
+
 import ctypes
 import csharptypes
+
+import VSMError
 
 path = "vocaloid editor path: "
 
 def load_vsm():
-    pass
+    global VIS_VSM_WVSMModuleIF_createManager
+    VIS_VSM_WVSMModuleIF_createManager = vsm[649]
+    VIS_VSM_WVSMModuleIF_createManager.argtypes = [csharptypes.LPWStr]
+    VIS_VSM_WVSMModuleIF_createManager.restype = csharptypes.IntPtr
+    global VIS_VSM_WVSMModuleIF_hasManager
+    VIS_VSM_WVSMModuleIF_hasManager = vsm[650]
+    VIS_VSM_WVSMModuleIF_hasManager.argtypes = [csharptypes.IntPtr]
+    VIS_VSM_WVSMModuleIF_hasManager.restype = ctypes.c_bool
+    global VIS_VSM_WVSMModuleIF_lastError
+    VIS_VSM_WVSMModuleIF_lastError = vsm[651]
+    VIS_VSM_WVSMModuleIF_lastError.argtypes = []
+    VIS_VSM_WVSMModuleIF_lastError.restype = ctypes.c_int
 
 def load_vsm_path():
-    pass
+    global vsm
+    os.chdir(path)
+    vsm = ctypes.cdll.LoadLibrary("vsm.dll")
+    load_vsm()
 
 def load_vsm_dll(vsmdll):
-    pass
+    global vsm
+    vsm = vsmdll
+    load_vsm()
+
+class WVSMModuleIF:
+
+    _managers = map()
+
+    def VIS_VSM_WVSMModuleIF_createManager(appId):
+        global VIS_VSM_WVSMModuleIF_createManager
+        return VIS_VSM_WVSMModuleIF_createManager(appId)
+
+    def VIS_VSM_WVSMModuleIF_hasManager(pSequenceManager):
+        global VIS_VSM_WVSMModuleIF_hasManager
+        return VIS_VSM_WVSMModuleIF_hasManager(pSequenceManager)
+
+    def VIS_VSM_WVSMModuleIF_lastError():
+        global VIS_VSM_WVSMModuleIF_lastError
+        return VIS_VSM_WVSMModuleIF_lastError()
+
+    def CreateManager(appID):
+        if(appID == None):
+            return None
+        if(appID == ""):
+            return None
+    
 
 if(__name__ == "__main__"):
     path = input(path)
