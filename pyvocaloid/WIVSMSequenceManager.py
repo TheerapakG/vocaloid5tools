9a4a9be46ad6942ea2d6ac58a63a93e14e0a5250
TheerapakG <theerapakg@gmail.com>
2018-08-26 21:39:44 +0700

    Modify WIVSMSequenceManager.py

--- pyvocaloid/WIVSMSequenceManager.py
+++ pyvocaloid/WIVSMSequenceManager.py
@@ -21,8 +21,42 @@ def load_vsm():
     VIS_VSM_WIVSMSequenceManager_createSequence.restype = csharptypes.IntPtr
     global VIS_VSM_WIVSMSequenceManager_openSequence
     VIS_VSM_WIVSMSequenceManager_openSequence = vsm[449]
-    VIS_VSM_WIVSMSequenceManager_openSequence.argtypes = [csharptypes.LPWStr, csharptypes.LPWStr, “pointer struct VSMSequenceData”]
+    VIS_VSM_WIVSMSequenceManager_openSequence.argtypes = [csharptypes.IntPtr, csharptypes.LPWStr, “pointer struct VSMSequenceData”]
     VIS_VSM_WIVSMSequenceManager_openSequence.restype = csharptypes.IntPtr
+    global VIS_VSM_WIVSMSequenceManager_openLegacySequence
+    VIS_VSM_WIVSMSequenceManager_openLegacySequence = vsm[448]
+    VIS_VSM_WIVSMSequenceManager_openLegacySequence.argtypes = [csharptypes.IntPtr, csharptypes.LPWStr, “pointer struct VSMSequenceData”, ctypes.c_uint, ctypes.c_bool]
+    VIS_VSM_WIVSMSequenceManager_openLegacySequence.restype = csharptypes.IntPtr
+    global VIS_VSM_WIVSMSequenceManager_lastErrorVsqParser
+    VIS_VSM_WIVSMSequenceManager_lastErrorVsqParser = vsm[447]
+    VIS_VSM_WIVSMSequenceManager_lastErrorVsqParser.argtypes = [csharptypes.IntPtr]
+    VIS_VSM_WIVSMSequenceManager_lastErrorVsqParser.restype = ctypes.c_uint
+    global VIS_VSM_WIVSMSequenceManager_lastErrorMessageVsqParser
+    VIS_VSM_WIVSMSequenceManager_lastErrorMessageVsqParser = vsm[446]
+    VIS_VSM_WIVSMSequenceManager_lastErrorMessageVsqParser.argtypes = [csharptypes.IntPtr]
+    VIS_VSM_WIVSMSequenceManager_lastErrorMessageVsqParser.restype = csharptypes.IntPtr
+    global VIS_VSM_WIVSMSequenceManager_hasSequence
+    VIS_VSM_WIVSMSequenceManager_hasSequence = vsm[444]
+    VIS_VSM_WIVSMSequenceManager_hasSequence.argtypes = [csharptypes.IntPtr, csharptypes.IntPtr]
+    VIS_VSM_WIVSMSequenceManager_hasSequence.restype = ctypes.c_bool
+    global VIS_VSM_WIVSMSequenceManager_createClipboard
+    VIS_VSM_WIVSMSequenceManager_createClipboard = vsm[439]
+    VIS_VSM_WIVSMSequenceManager_createClipboard.argtypes = [csharptypes.IntPtr]
+    VIS_VSM_WIVSMSequenceManager_createClipboard.restype = csharptypes.IntPtr
+    global VIS_VSM_WIVSMSequenceManager_databaseManager
+    VIS_VSM_WIVSMSequenceManager_databaseManager = vsm[441]
+    VIS_VSM_WIVSMSequenceManager_databaseManager.argtypes = [csharptypes.IntPtr]
+    VIS_VSM_WIVSMSequenceManager_databaseManager.restype = csharptypes.IntPtr
+    global VIS_VSM_WIVSMSequenceManager_setDatabaseManager
+    VIS_VSM_WIVSMSequenceManager_setDatabaseManager = vsm[451]
+    VIS_VSM_WIVSMSequenceManager_setDatabaseManager.argtypes = [csharptypes.IntPtr, csharptypes.IntPtr]
+    global VIS_VSM_WIVSMSequenceManager_dseManager
+    VIS_VSM_WIVSMSequenceManager_dseManager = vsm[443]
+    VIS_VSM_WIVSMSequenceManager_dseManager.argtypes = [csharptypes.IntPtr]
+    VIS_VSM_WIVSMSequenceManager_dseManager.restype = csharptypes.IntPtr
+    global VIS_VSM_WIVSMSequenceManager_setDSEManager
+    VIS_VSM_WIVSMSequenceManager_setDSEManager = vsm[450]
+    VIS_VSM_WIVSMSequenceManager_setDSEManager.argtypes = [csharptypes.IntPtr, csharptypes.IntPtr]
 
 def load_vsm_path():
     global vsm
@@ -52,10 +86,63 @@ class WIVSMSequenceManager:
         global VIS_VSM_WIVSMSequenceManager_destroy
         return VIS_VSM_WIVSMSequenceManager_destroy(cppobjptr)
 
-    def VIS_VSM_WIVSMSequenceManager_createSequence(cppobjptr, sequenceData):
+    def VIS_VSM_WIVSMSequenceManager_createSequence(cppobjptr, sequenceData_p):
         global VIS_VSM_WIVSMSequenceManager_createSequence
-        return VIS_VSM_WIVSMSequenceManager_createSequence(cppobjptr, sequenceData)
+        return VIS_VSM_WIVSMSequenceManager_createSequence(cppobjptr, sequenceData_p)
 
-    def VIS_VSM_WIVSMSequenceManager_openSequence(filePath, vsqxSchemaDirPath, sequenceData):
+    def VIS_VSM_WIVSMSequenceManager_openSequence(filePath, vsqxSchemaDirPath, sequenceData_p):
         global VIS_VSM_WIVSMSequenceManager_openSequence
-        return VIS_VSM_WIVSMSequenceManager_openSequence(filePath, vsqxSchemaDirPath, sequenceData)
+        return VIS_VSM_WIVSMSequenceManager_openSequence(filePath, vsqxSchemaDirPath, sequenceData_p)
+
+    def VIS_VSM_WIVSMSequenceManager_openLegacySequence(cppobjptr, path, sequenceData_p, codePage, channelAsTrack):
+        global VIS_VSM_WIVSMSequenceManager_openLegacySequence
+        return VIS_VSM_WIVSMSequenceManager_openLegacySequence(cppobjptr, path, sequenceData_p, codePage, channelAsTrack)
+
+    def VIS_VSM_WIVSMSequenceManager_lastErrorVsqParser(cppobjptr):
+        global VIS_VSM_WIVSMSequenceManager_lastErrorVsqParser
+        return VIS_VSM_WIVSMSequenceManager_lastErrorVsqParser(cppobjptr)
+
+    def VIS_VSM_WIVSMSequenceManager_lastErrorMessageVsqParser(cppobjptr):
+        global VIS_VSM_WIVSMSequenceManager_lastErrorMessageVsqParser
+        return VIS_VSM_WIVSMSequenceManager_lastErrorMessageVsqParser(cppobjptr)
+
+    def VIS_VSM_WIVSMSequenceManager_hasSequence(cppobjptr, sequence):
+        global VIS_VSM_WIVSMSequenceManager_hasSequence
+        return VIS_VSM_WIVSMSequenceManager_hasSequence(cppobjptr, sequence)
+
+    def VIS_VSM_WIVSMSequenceManager_createClipboard(cppobjptr):
+        global VIS_VSM_WIVSMSequenceManager_createClipboard
+        return VIS_VSM_WIVSMSequenceManager_createClipboard(cppobjptr)
+
+    def VIS_VSM_WIVSMSequenceManager_databaseManager(cppobjptr):
+        global VIS_VSM_WIVSMSequenceManager_databaseManager
+        return VIS_VSM_WIVSMSequenceManager_databaseManager(cppobjptr)
+
+    def VIS_VSM_WIVSMSequenceManager_setDatabaseManager(cppobjptr, databaseManager):
+        global VIS_VSM_WIVSMSequenceManager_setDatabaseManager
+        return VIS_VSM_WIVSMSequenceManager_setDatabaseManager(cppobjptr, databaseManager)
+
+    def VIS_VSM_WIVSMSequenceManager_dseManager(cppobjptr):
+        global VIS_VSM_WIVSMSequenceManager_dseManager
+        return VIS_VSM_WIVSMSequenceManager_dseManager(cppobjptr)
+
+    def VIS_VSM_WIVSMSequenceManager_setDSEManager(cppobjptr, dseManager):
+        global VIS_VSM_WIVSMSequenceManager_setDSEManager
+        return VIS_VSM_WIVSMSequenceManager_setDSEManager(cppobjptr, dseManager)
+
+    def IntPtr(obj):
+        return obj._cppObjPtr
+
+    def Equals(self, obj):
+        if (obj == None):
+            return False
+        try:
+            wivsmSequenceManager = type(self)(obj)
+        except (TypeError, ValueError) as e:
+            return False
+        if (wivsmSequenceManager == None):
+            return False
+        return self._cppObjPtr == wivsmSequenceManager._cppObjPtr
+
+    def GetHashCode(self):
+        return self._cppObjPtr
