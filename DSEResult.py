13932d9c17be899ced11133b40a94e04df9afe79
Theerapak Gomolmanee <theerapak_g@hotmail.com>
2018-09-03 23:51:13 +0700

    Revert "Add files via upload"
    This reverts commit e7b9b5d28f72b9b607b5b17a2e8fbb3df635b975.

--- DSEResult.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, print_function, unicode_literals
-__metaclass__ = type
-
-import enum
-
-class DSEResult(enum.IntEnum):
-    Error = -1
-    Successful = 0
