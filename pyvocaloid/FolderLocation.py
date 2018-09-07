a76d033421413790967039457bf2ef0045681f5c
TheerapakG <theerapakg@gmail.com>
2018-09-07 11:22:12 +0700

    Update FolderLocation.py

--- pyvocaloid/FolderLocation.py
+++ pyvocaloid/FolderLocation.py
@@ -2,38 +2,41 @@
 import os
 
 class FolderLocation:
-    PathSystemData = os.environ[“CommonProgramFiles”] + "VOCALOID5"
-    public static readonly string PathSystemStylePreset = Path.Combine(FolderLocation.PathSystemData, "StylePreset")
-    public static readonly string PathSystemMedia = Path.Combine(FolderLocation.PathSystemData, "Media")
-    public static readonly string PathSystemMESingingSkill = Path.Combine(FolderLocation.PathSystemData, "MIDIEffect", "SingingSkill")
-    public static readonly string PathSystemMERobotVoice = Path.Combine(FolderLocation.PathSystemData, "MIDIEffect", "RobotVoice")
-    public static readonly string PathSystemVoicelib = Path.Combine(FolderLocation.PathSystemData, "Voicelib")
-    public static readonly string PathSystemAttackReleaselib = Path.Combine(FolderLocation.PathSystemData, "AttackReleaselib")
-    public static readonly string PathSystemExplib = Path.Combine(FolderLocation.PathSystemData, "Explib")
-    public static readonly string PathSystemRscVoice = Path.Combine(FolderLocation.PathSystemData, "Resource", "Voice")
-    public static readonly string PathSystemRscARIcon = Path.Combine(FolderLocation.PathSystemData, "Resource", "AttackRelease", "Icon")
-    public static readonly string PathSystemRscARPreview = Path.Combine(FolderLocation.PathSystemData, "Resource", "AttackRelease", "Preview")
-    private static readonly string PathUserData = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "VOCALOID5")
-    public static readonly string PathUserStylePreset = Path.Combine(FolderLocation.PathUserData, "StylePreset")
-    public static readonly string PathUserMedia = Path.Combine(FolderLocation.PathUserData, "Media")
-    public static readonly string PathUserMESingingSkill = Path.Combine(FolderLocation.PathUserData, "MIDIEffect", "SingingSkill")
-    public static readonly string PathUserUdic = Path.Combine(FolderLocation.PathUserData, "Udic")
-    public static readonly string PathUserShortcut = Path.Combine(FolderLocation.PathUserData, "Shortcut")
-    public static readonly string PathUserContentsList = Path.Combine(FolderLocation.PathUserData, "Contents")
-    private const string fNameApp = "VOCALOID5"
-    private const string fNameStylePreset = "StylePreset"
-    private const string fNameMedia = "Media"
-    private const string fNameMidiEffect = "MIDIEffect"
-    private const string fNameSingingSkill = "SingingSkill"
-    private const string fNameRobotVoice = "RobotVoice"
-    private const string fNameVoicelib = "Voicelib"
-    private const string fNameAttackReleaselib = "AttackReleaselib"
-    private const string fNameExplib = "Explib"
-    private const string fNameResource = "Resource"
-    private const string fNameVoice = "Voice"
-    private const string fNameAttackRelease = "AttackRelease"
-    private const string fNameIcon = "Icon"
-    private const string fNamePreview = "Preview"
-    private const string fNameUdic = "Udic"
-    private const string fNameShortcut = "Shortcut"
-    private const string fNameContentsList = "Contents"
+    PathSystemData = os.environ["CommonProgramFiles"] + "\\VOCALOID5"
+    PathUserData = os.environ["ApplicationData"] + "\\VOCALOID5"
+
+    fNameApp = "VOCALOID5"
+    fNameStylePreset = "StylePreset"
+    fNameMedia = "Media"
+    fNameMidiEffect = "MIDIEffect"
+    fNameSingingSkill = "SingingSkill"
+    fNameRobotVoice = "RobotVoice"
+    fNameVoicelib = "Voicelib"
+    fNameAttackReleaselib = "AttackReleaselib"
+    fNameExplib = "Explib"
+    fNameResource = "Resource"
+    fNameVoice = "Voice"
+    fNameAttackRelease = "AttackRelease"
+    fNameIcon = "Icon"
+    fNamePreview = "Preview"
+    fNameUdic = "Udic"
+    fNameShortcut = "Shortcut"
+    fNameContentsList = "Contents"
+
+FolderLocation.PathSystemStylePreset = FolderLocation.PathSystemData + "\\StylePreset"
+FolderLocation.PathSystemMedia = FolderLocation.PathSystemData + "\\Media"
+FolderLocation.PathSystemMESingingSkill = FolderLocation.PathSystemData + "\\MIDIEffect" + "\\SingingSkill"
+FolderLocation.PathSystemMERobotVoice = FolderLocation.PathSystemData + "\\MIDIEffect" + "\\RobotVoice"
+FolderLocation.PathSystemVoicelib = FolderLocation.PathSystemData + "\\Voicelib"
+FolderLocation.PathSystemAttackReleaselib = FolderLocation.PathSystemData + "\\AttackReleaselib"
+FolderLocation.PathSystemExplib = FolderLocation.PathSystemData + "\\Explib"
+FolderLocation.PathSystemRscVoice = FolderLocation.PathSystemData + "\\Resource" + "\\Voice"
+FolderLocation.PathSystemRscARIcon = FolderLocation.PathSystemData + "\\Resource" + "\\AttackRelease" + "\\Icon"
+FolderLocation.PathSystemRscARPreview = FolderLocation.PathSystemData + "\\Resource" + "\\AttackRelease" + "\\Preview"
+
+FolderLocation.PathUserStylePreset = FolderLocation.PathUserData + "\\StylePreset"
+FolderLocation.PathUserMedia = FolderLocation.PathUserData + "\\Media"
+FolderLocation.PathUserMESingingSkill = FolderLocation.PathUserData + "\\MIDIEffect" + "\\SingingSkill"
+FolderLocation.PathUserUdic = FolderLocation.PathUserData + "\\Udic"
+FolderLocation.PathUserShortcut = FolderLocation.PathUserData + "\\Shortcut"
+FolderLocation.PathUserContentsList = FolderLocation.PathUserData + "\\Contents"
